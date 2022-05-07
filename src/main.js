5ca361570053381796bd3ef21658d3597f114b40
zhuzhuxia <gmail@888.com>
2022-05-07 11:34:08 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+//引入路由
+import router from '@/router'
 new Vue({
   render: h => h(App),
+  //注册路由:底下的写法是KV一致省略V[router小写的]
+  //注册路由信息，当这里书写router的时候，组件身上都拥有$route,$router属性
+  router
 }).$mount('#app')
